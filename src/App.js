854216a5b92b9de3a88a6f159f9a1d1dcb132243
Sean Stephenson <seanmichaelstephenson@gmail.com>
2017-10-25 15:14:48 -0700

    Update App.js
    clean up

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Redux Demo
         </p>
-        <h2>Redux Demo</h2>
+        <hr />
         <ItemList />
       </div>
     );
